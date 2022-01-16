b9c4a7a4e115e7ed2d6b1dd3ca39e1ec218ac3e9
DoeunKongden <doeunkongden1@gmail.com>
2022-01-16 22:56:13 +0700

    Added header

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -5,10 +5,40 @@ function Header() {
     return (
         <Nav>
 
-          <Logo src='' />
+          <Logo src='./Disney Clone Challenge/images/logo.svg' />
             <NavMenu>
+                <a>
+                    <img src='./Disney Clone Challenge/images/home-icon.svg' />
+                    <span>Home</span>
+                </a>
 
+                <a>
+                    <img src='./Disney Clone Challenge/images/search-icon.svg' />
+                    <span>Search</span>
+                </a>
+
+                <a>
+                    <img src='./Disney Clone Challenge/images/watchlist-icon.svg' />
+                    <span>Watch list</span>
+                </a>
+
+                <a>
+                    <img src='./Disney Clone Challenge/images/original-icon.svg' />
+                    <span>original</span>
+                </a>
+
+                <a>
+                    <img src='./Disney Clone Challenge/images/movie-icon.svg' />
+                    <span>Movies</span>
+                </a>
+
+                <a>
+                    <img src='./Disney Clone Challenge/images/series-icon.svg' />
+                    <span>Series</span>
+                </a>
             </NavMenu>
+
+            <UserImg src='./my pf.jpg' />
 
         </Nav>
     )
@@ -20,6 +50,9 @@ const  Nav = styled.nav`
     height: 70px;
     background-color: #090b13;
     color: white;
+    display: flex;
+    align-items: center;
+    padding: 0 36px;
 `
 const Logo = styled.img`
     width: 80px;
@@ -27,5 +60,55 @@ const Logo = styled.img`
 `
 
 const NavMenu = styled.div`
-    
+    display: flex;
+    flex: 1;
+    margin-left: 20px;
+    align-items: center;
+
+    a{
+        display: flex;
+        align-items: center;
+        padding: 0 12px;
+        cursor: pointer;
+
+        img{
+            height: 20px;
+        }
+
+        span{
+            font-size: 13px;
+            letter-spacing: 1.42px;
+            position: relative;
+
+            &:after{
+                content: "";
+                height:  2px;
+                background-color: white;
+                position: absolute;
+                left: 0;
+                right: 0;
+                bottom: -6px;
+                opacity: 0;
+                transform-origin: left center;
+                transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+                transform: scaleX(0);
+            }
+        }
+
+        &:hover{
+            span:after{
+                transform: scaleX(1);
+                opacity: 1;
+            }
+        }
+    }
+
+
+`
+
+const UserImg = styled.img`
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    cursor: pointer;
 `
