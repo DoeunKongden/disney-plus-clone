8007d4479d29a564ee1e6268f98e96bc373fe412
DoeunKongden <doeunkongden1@gmail.com>
2022-01-21 14:29:09 +0700

    Finished project on Disney plus

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -3,17 +3,20 @@ import styled from 'styled-components'
 import ImgSlider from './ImgSlider'
 import Movies from './Movies'
 import Viewers from './Viewers'
-import db from '../firebase'
+
 
 
 function Home() {
 
-    useEffect(()=>{
-        db.collection("movies").onSnapshot((snapshot)=> {
-            console.log(snapshot);
-        }) // the thing inside useEffect will alwasy run when ever the home page is loading
-    },[])
+    // useEffect(()=>{
+    //     db.collection("movies").onSnapshot((snapshot)=> {
+    //         console.log(snapshot);
+    //     }) // the thing inside useEffect will alwasy run when ever the home page is loading
+    // },[])  
 
+ //since my the firebase data base that nanz provide us has change there security authentication , we won't be able to do our backend for this data base
+ 
+    
     return (
         <Container>
             <ImgSlider></ImgSlider>
